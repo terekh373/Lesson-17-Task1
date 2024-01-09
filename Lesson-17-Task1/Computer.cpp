7163d544e476602c52ca71d6332df2a84f2ef226
Tereshchenko Anton <Teres_vm10@student.itstep.org>
2024-01-09 16:52:41 +0100

    definition methods

--- Lesson-17-Task1/Computer.cpp
+++ Lesson-17-Task1/Computer.cpp
@@ -1,1 +1,24 @@
 #include "Computer.h"
+
+Computer::Computer(string name, float frequency, int ram, bool dvd, float cost)
+{
+	this->name = name;
+	this->frequency = frequency;
+	this->ram = ram;
+	this->dvd = dvd;
+	this->cost = cost;
+}
+
+int Computer::getRam() const
+{
+    return ram;
+}
+
+void Computer::show() const
+{
+	cout << "Name: " << name << endl;
+	cout << "Frequency: " << frequency << "GHz" << endl;
+	cout << "RAM: " << ram << "Gb" << endl;
+	cout << "DVD: " << dvd << endl;
+	cout << "Cost: " << cost << "UAH" << endl;
+}
